9d1b337c45566f6cc290ed834232d6f22ee90360
seventyfor <1054637061@qq.com>
2018-12-28 11:50:22 +0800

    Wtf (#9)
    * OMG
    
    * new submit:add query module
    
    * Update House.java
    
    Delete eid, Update Type
    
    * Update House.java

--- src/cn/action/QueryAction.java
+++ src/cn/action/QueryAction.java
@@ -14,20 +14,57 @@ public class QueryAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		String method=request.getParameter("method");
+		String result = "";
+		
+		switch(method) {
+			case "showAllHouse":result = showAllHouse(request,response);break;
+			case "searchHouseByDid":result = searchHouse(request,response);break;
+			case "showOneHouse":result = showOneHouse(request,response);break;
+		}
+		return result; 
+	} 
+	
+	public String showOneHouse(HttpServletRequest request, HttpServletResponse response) {
 		HouseDao hd=new HouseDaoImpl();
-		String htid=request.getParameter("htid");
-		String tid=request.getParameter("tid");
-		String price=request.getParameter("price");
-		String[] propertyname={"htid","tid","price"};
-		Object[] value={htid,tid,price};
+		String hid=request.getParameter("hid");
+		int hid_value = Integer.parseInt(hid);
+		String[] propertyname={"hid"};
+		Object[] value={hid_value};
+		
 		try {
 			List<House> list = hd.housesearch(propertyname, value);
-			request.setAttribute("list", list);
-		} catch (Exception e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		}
-		return "query.jsp";
+			request.setAttribute("houseList", list);
+		} catch (Exception e) { e.printStackTrace(); }
+		
+		return "showOneHouse.jsp";
+	}
+
+	public String searchHouse(HttpServletRequest request, HttpServletResponse response) {
+		HouseDao hd=new HouseDaoImpl();
+		String district=request.getParameter("district");
+		
+		try {
+			List<House> list = hd.houseSearchByDistrict(district);
+			request.setAttribute("houseList", list);
+		} catch (Exception e) {e.printStackTrace();}
+		
+		
+		return "showAllHouse.jsp";
+	}
+
+	public String showAllHouse(HttpServletRequest request, HttpServletResponse response) {
+		HouseDao hd=new HouseDaoImpl();
+		int status = 0;
+		int reviewed = 1;
+		String[] propertyname={"status","reviewed"};
+		Object[] value={status,reviewed};
+		try {
+			List<House> list = hd.housesearch(propertyname, value);
+			request.setAttribute("houseList", list);
+		} catch (Exception e) { e.printStackTrace(); }
+		return "showAllHouse.jsp";
 	}
 
 }
--- src/cn/dao/HouseDao.java
+++ src/cn/dao/HouseDao.java
@@ -6,21 +6,11 @@ import cn.bean.House;
 
 public interface HouseDao {
 
-	//1.发布房源信息
 	public boolean doHouse(House ho);
-	//2.修改房源信息
 	public boolean updateHouse(House ho);
-
-	 //5.修改房屋状态1（经纪人）
-	 public boolean updateHouseByStatus(House ho,int status);
-	 
-	 /*
-	// 7.修改房屋状态2（管理员）
-	 public boolean updateHouseByAuditing(House ho,String auditing);
-	 */
-	 
-	// 8.修改房屋信息（管理员给房屋分配经纪人）
-	 public boolean updateHouseByLid(House ho,int lid);
-	 //多条件查询
-	 public List<House> housesearch(String[] propertyname,Object[] value) throws Exception;
+	public boolean updateHouseByStatus(House ho,int status);
+	public boolean updateHouseByLid(House ho,int lid);
+	public List<House> housesearch(String[] propertyname,Object[] value) throws Exception;
+	//search house by district
+	public List<House> houseSearchByDistrict(String district) throws Exception;
 }
