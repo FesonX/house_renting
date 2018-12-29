685dee86860a58c2d06e61b70e5ce55d32a9fde4
FesonX <fesonx@foxmail.com>
2018-12-29 09:12:47 +0800

    Fix Messy Code

--- src/cn/dao/LandlordDao.java
+++ src/cn/dao/LandlordDao.java
@@ -5,21 +5,20 @@ import java.util.List;
 import cn.bean.Landlord;
 
 public interface LandlordDao {
-	//1.��¼
-	public Landlord login(String name,String password);
-	//2.�޸�����
+	//1.登录
+	public Landlord login(int lid,String password);
+	//2.修改密码
 	public boolean updatePwd(Landlord ag,String password);
-	//3.��ӷ���
+	//3.添加房东
 	public boolean doLandlord(Landlord ag);
-	//4.�޸ķ���
+	//4.修改房东
 	public boolean updateLandlord(Landlord ag);
-	//5.ɾ������
+	//5.删除房东
 	public boolean deleteLandlord(int lid);
-	//6.ͨ��������ѯ����
+	//6.通过镇区查询房东
 	public List<Landlord> findLandlordByDid(int did);
-	//7.��ѯȫ������
+	//7.查询全部房东
 	public List<Landlord> findLandlord();
-	//8.��ѯĳ������
-	public Landlord findLandlordBy(int lid);
-	boolean register(Landlord ag);
+	//8.查询某个房东
+	public Landlord findLandlordBy(String lid);
 }
