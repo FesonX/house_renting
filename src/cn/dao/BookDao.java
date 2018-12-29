a653e1ee168ba98c5ee985a7a3400a6188bd9a48
FesonX <fesonx@foxmail.com>
2018-12-29 08:51:20 +0800

    Fix Messy Code

--- src/cn/dao/BookDao.java
+++ src/cn/dao/BookDao.java
@@ -5,11 +5,8 @@ import java.util.List;
 import cn.bean.Book;
 
 public interface BookDao {
-	//1.ԤԼ�����ˣ����ԤԼ��Ϣ��
 	public boolean doBook(Book bo);
-	//2.�鿴ԤԼ��Ϣ��(�û�)
 	public List<Book> findBookByRid(String rid);
-	//3.�鿴ԤԼ��Ϣ�������ˣ�
 	public List<Book> findBookByLid(String lid);
 	public int countBookSize();
 }
