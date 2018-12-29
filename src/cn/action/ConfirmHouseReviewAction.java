aade67ed36114e67c50d1cbac57f57f764faf30c
FesonX <fesonx@foxmail.com>
2018-12-29 16:46:05 +0800

    [Add] Confirm House Reviewed Function

---
+++ src/cn/action/ConfirmHouseReviewAction.java
@@ -0,0 +1,45 @@
+package cn.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.dao.HouseDao;
+import cn.dao.impl.HouseDaoImpl;
+import cn.framework.Action;
+import cn.bean.House;
+
+public class ConfirmHouseReviewAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		boolean result = false;
+		String msg = "";
+		result = confirmHouseReviewAction(request, response);
+		if(result==true) {
+			msg = "success";
+			request.setAttribute("message", msg);
+		}
+		else {
+			msg = "failure";
+			request.setAttribute("message", msg);
+		}
+		return "houseReview.do?msg="+msg;
+	}
+	
+	public boolean confirmHouseReviewAction(HttpServletRequest request, HttpServletResponse response) {
+		HouseDao hd = new HouseDaoImpl();
+		String hid = request.getParameter("hid");
+		int hid_value = Integer.parseInt(hid);
+		String[] propertyname={"hid"};
+		Object[] value={hid_value};
+		boolean result = false;
+		
+		try {
+			List<House> list = hd.housesearch(propertyname, value);
+			result = hd.updateHouseByReview(list.get(0), 1);
+		}catch (Exception e) {e.printStackTrace();}	
+		return result;
+	}
+}
